178ca05e3ed6031b0a49d9995d203a4cbc402874
Dev <devnarayantajpuriya12@gmail.com>
2023-07-15 15:00:42 +0545

    big-changes

--- LatestNews/models.py
+++ LatestNews/models.py
@@ -4,81 +4,84 @@ from django.db import models
 
 
 class Item(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image_url = models.URLField()
-    link = models.URLField()
+    title = models.CharField(max_length=1000)
+    description = models.TextField(default=None, null=True)
+    image_url = models.URLField(max_length=2000, null=True)
+    link = models.URLField(max_length=2000)
 
     def __str__(self):
         return self.title
     
     
 class sportsNews(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image_url = models.URLField(max_length=2000)
+    title = models.CharField(max_length=1000)
+    description = models.TextField(default=None, null=True)
+    image_url = models.URLField(max_length=2000, null=True)
     link = models.URLField(max_length=2000)
 
     def __str__(self):
         return self.title
     
 class politicalNews(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image_url = models.URLField()
-    link = models.URLField()
+    title = models.CharField(max_length=1000)
+    description = models.TextField(default=None, null=True)
+    image_url = models.URLField(max_length=2000, null=True)
+    link = models.URLField(max_length=2000)
 
     def __str__(self):
         return self.title
 class upatiyaka(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image_url = models.URLField()
-    link = models.URLField()
+    title = models.CharField(max_length=1000)
+    description = models.TextField(default=None, null=True)
+    image_url = models.URLField(max_length=2000, null=True)
+    link = models.URLField(max_length=2000)
 
     def __str__(self):
         return self.title
     
 class worldNews(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image_url = models.URLField()
-    link = models.URLField()
+    title = models.CharField(max_length=1000)
+    description = models.TextField(default=None, null=True)
+    image_url = models.URLField(max_length=2000, null=True)
+    link = models.URLField(max_length=2000)
 
     def __str__(self):
         return self.title
     
     
 class monoranjan(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image_url = models.URLField()
-    link = models.URLField()
+    title = models.CharField(max_length=1000)
+    description = models.TextField(default=None, null=True)
+    image_url = models.URLField(max_length=2000, null=True)
+    link = models.URLField(max_length=2000)
 
     def __str__(self):
         return self.title
     
 class bigyanPrabidhi(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image_url = models.URLField()
-    link = models.URLField()
+    title = models.CharField(max_length=1000)
+    description = models.TextField(default=None, null=True)
+    image_url = models.URLField(max_length=2000, null=True)
+    link = models.URLField(max_length=2000)
 
     def __str__(self):
         return self.title
     
 class bichar(models.Model):
-    title = models.CharField(max_length=100)
-    image_url = models.URLField()
-    link = models.URLField()
+    title = models.CharField(max_length=1000)
+    description = models.TextField( default=None, null=True)
+    image_url = models.URLField(max_length=2000, null=True)
+    link = models.URLField(max_length=2000)
 
     def __str__(self):
         return self.title
     
     
 class topheadlines(models.Model):
-    title = models.CharField(max_length=100)
-    link = models.URLField()
+    title = models.CharField(max_length=1000)
+    description = models.TextField(default=None, null=True)
+    image_url = models.URLField(max_length=2000, null=True)
+    link = models.URLField(max_length=2000)
 
     def __str__(self):
         return self.title
--- LatestNews/views.py
+++ LatestNews/views.py
@@ -35,3 +35,99 @@ def index(request):
      
     
     return render(request, 'index.html', context)
+
+def get_model_from_file_name(file_name):
+    # Define the mapping between file names and models
+    model_mapping = {
+        'politicalnews.csv': politicalNews,
+        'topheadlines.csv': topheadlines,
+        'sportnews.csv': sportsNews,
+        'worldnews.csv': worldNews,
+        'upatiyakas.csv': upatiyaka,
+        'bigyanprabidhis.csv': bigyanPrabidhi,
+        'bichars.csv': bichar,
+        'lokpriyas.csv': Item,
+        'monoranjans.csv': monoranjan,
+    }
+
+    # Get the corresponding model based on the file name
+    return model_mapping.get(file_name, None)
+
+
+def cvstodatabase(csv_file):
+    model = get_model_from_file_name(csv_file.name)
+    if model == politicalNews:
+        politicalNews.objects.all().delete()
+    elif model == topheadlines:
+        topheadlines.objects.all().delete()
+    elif model == sportsNews:
+        sportsNews.objects.all().delete()
+    elif model == monoranjan:
+        monoranjan.objects.all().delete()
+    elif model == upatiyaka:
+        upatiyaka.objects.all().delete()
+    elif model == bigyanPrabidhi:
+        bigyanPrabidhi.objects.all().delete()
+    elif model == bichar:
+        bichar.objects.all().delete()
+    elif model == worldNews:
+        worldNews.objects.all().delete()
+    elif model == Item:
+        Item.objects.all().delete()
+        
+    
+    with open('static/media/' + csv_file.name, 'r', encoding='utf-8-sig') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            # Assuming the CSV file has two columns: 'name' and 'email'
+            title = row[0]
+            link = row[1]
+            description = row[2]
+            image_url = row[3]
+            
+            # Create a new CSVData object and save it to the database
+            if model == politicalNews:
+                csv_data = politicalNews(title=title, description=description, image_url=image_url, link=link)
+                csv_data.save()
+            elif model == topheadlines:
+                csv_data = topheadlines(title=title, description=description, image_url=image_url, link=link)
+                csv_data.save()
+            elif model == sportsNews:
+                csv_data = sportsNews(title=title, description=description, image_url=image_url, link=link)
+                csv_data.save()
+            elif model == monoranjan:
+                csv_data = monoranjan(title=title, description=description, image_url=image_url, link=link)
+                csv_data.save()
+            elif model == bichar:
+                csv_data = bichar(title=title, description=description, image_url=image_url, link=link)
+                csv_data.save()
+            elif model == bigyanPrabidhi:
+                csv_data = bigyanPrabidhi(title=title, description=description, image_url=image_url, link=link)
+                csv_data.save()
+            elif model == worldNews:
+                csv_data = worldNews(title=title, description=description, image_url=image_url, link=link)
+                csv_data.save()
+            elif model == Item:
+                csv_data = Item(title=title, description=description, image_url=image_url, link=link)
+                csv_data.save()
+            elif model == upatiyaka:
+                csv_data = upatiyaka(title=title, description=description, image_url=image_url, link=link)
+                csv_data.save()
+
+                    
+
+def files(request):
+    if request.method == 'POST' and request.FILES['csvFile']:
+        csv_file = request.FILES['csvFile']
+        # Process the CSV file here (e.g., save it, read data, etc.)
+        # Replace this with your actual file processing logic
+        # Example: Save the file in the 'media' directory
+        with open('static/media/' + csv_file.name, 'wb') as file:
+            for chunk in csv_file.chunks():
+                file.write(chunk)
+                
+        cvstodatabase(csv_file)
+    
+    return render(request, 'fileUpload.html')
+
+
